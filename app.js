38e32e90d0490d470522ba58597ecb20303795f3
Alshakarchi Hani - Komvux Kärnan <hani.alshakarchi@skola.helsingborg.se>
2019-10-29 13:32:30 +0100

    pseudo

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 $(() => {
   
     $('.top h1').text('HANI');
+    // Rubrik.
     $('#list').children().text('Hani.');
+    // Listan, "varor" som läggs till.
     $('.element').children().addClass('hidden');
+    // "Varorna" som är gömda tills man klickar på "lägg till".
   
     $('.change-show').click(() => {
       $('.show').toggleClass('hidden');
     });
   
     $('.add-item').click(() => {
+      // Själva "Lägg till"knappen.
       $('#list').append('<li>Hani.</li>');
     });
   
